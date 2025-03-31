b3b6535799577cfc1e72168a1182c0ab5b83f44e
Barbara <5elementszone@gmail.com>
2025-03-31 21:05:41 +0200

    added windchill

--- scripts/place.js
+++ scripts/place.js
@@ -1,4 +1,22 @@
-import '../styles/place.css';
-import '../styles/components/header.css';
-import '../styles/components/main.css';
-import '../styles/utils.css';
+window.addEventListener("DOMContentLoaded", () => {
+    const temperature = 8; // degrees Celsius
+    const windSpeed = 10;
+    
+    const windChillElement = document.getElementById("WindChill");
+
+    function calculateWindChill(temp, speed) {
+        return (
+            13.12 +
+            0.6215 * temp -
+            11.37 * Math.pow(speed, 0.16) +
+            0.3965 * temp * Math.pow(speed, 0.16)
+        ).toFixed(1);
+    }
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+        const chill = calculateWindChill(temperature, windSpeed);
+        windChillElement.textContent = `${chill} °C`;
+      } else {
+        windChillElement.textContent = "N/A";
+    }
+});
